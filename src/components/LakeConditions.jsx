8bd1a855879b7c1e248856271ffb52790674f044
maksimovich.sam <maksimovich.sam@gmail.com>
2022-02-22 20:37:49 -0800

    Changed layout and add legend

--- src/components/LakeConditions.jsx
+++ src/components/LakeConditions.jsx
@@ -4,21 +4,22 @@ import "./styles/LakeConditions.css";
 function LakeConditions(props) {
   return (
     <div className="content-wrapper">
-      <div className="page-title"> Lake Conditions </div>
+        <div className='image-container'>
+            <img src="lake-two.jpg"></img>
+            <div className='page-description-container'>
+                <div className='page-description-title'> Lake Conditions </div>
+                <div className='page-description'>
+                    View lake forecasts
+                </div>
+                <div className='page-last-updated'> Last updated at 6:05 PM February 22, 2022</div>
+            </div>
+        </div>
 
-      <div className="center-content">
-        <CurrentChart width={800} height={600}/>
-      </div>
-
-      <div className="Map1">
-        <img src="LTPic.png" className="Pic" />
-        <img src="LTPic2.png" />
-      </div>
-
-      <div className="box">
-        <p>Current Lake Conditions:</p>
-
-        <p>Last Update:</p>
+      <div className="model-figure-container">
+        <div className="fig-container">
+            <img src="LTPic.png" className="Pic" />
+        </div>
+        <CurrentChart height={600}/>
       </div>
 
     </div>
--- src/components/RealTime/RealTimeConditions.jsx
+++ src/components/RealTime/RealTimeConditions.jsx
@@ -75,10 +75,11 @@ function RealTimeConditions(props) {
       <div className="content-wrapper">
 
         <div className='image-container'>
-            <img src="lake-one.jpg"></img>
+            <img src="lake-one.jpg" alt="Lake Tahoe"></img>
             <div className='page-description-container'>
                 <div className='page-description-title'> Real Time Conditions </div>
                 <div className='page-description'> Explore Lake Tahoe conditions in real time, routinely updated in twenty minute intervals. </div>
+                <div className='page-last-updated'> Last updated at 6:05 PM February 22, 2022</div>
             </div>
         </div>
 
