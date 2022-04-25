0936d3434d44342daf7fa50dba1eaac1e9510980
maksimovich.sam <maksimovich.sam@gmail.com>
2022-04-25 14:23:51 -0700

    Fixed wave height figure to fetch from s3

--- src/components/WaveHeightChart/wh_api.js
+++ src/components/WaveHeightChart/wh_api.js
@@ -1,7 +1,18 @@
 import { apply } from "../util";
 
-const base_url = "/api/waveheight/";
+const base_url = "https://lake-tahoe-conditions.s3.us-west-2.amazonaws.com/";
 const METER_TO_FEET = 3.28084;
+
+/* Wave Heights are stored in Amazon S3
+*
+* Each file located at the base url is formatted in as
+    'H_ws{x}_wd{y}.json' where x and y are wind speed and wind direction 
+    wind speed is in m/s rounded to integer in [0, 1, 2, ... 20]
+    wind direction is in degrees rounded to the nearest multiple of 5 in [0, 5, 10, ... 360]
+    
+    Example: waveheight/H_ws4_wd25.json => ws = 4 m/s, wd = 25 deg
+ *
+ */
 
 const wh_cache = {};
 
@@ -16,25 +27,20 @@ export async function retrieve_wh(wind_speed, wind_direction) {
         return wh_cache[key];
     }
 
-    let url = `${base_url}${wind_speed},${wind_direction}`;
+    // Round down to multiple of 1, and 5
+    let ws = Math.floor(wind_speed);
+    let wd = Math.floor(wind_direction / 5) * 5;
+
+    let url = `${base_url}waveheight/H_ws${ws}_wd${wd}.json`;
     let wh_data = await fetch(url, {
         method: "GET",
-        mode: "same-origin",
-        headers: {
-            "Content-Type": 'application/json'
-        }
+        mode: "cors"
     });
     wh_data = await wh_data.json();
-
-    if (wh_data === undefined || !('data' in wh_data))
-        throw new Error("Retrieved wave height matrix, but it's in an unexpected format");
-
-    wh_data = wh_data['data'];
 
     // Convert wave height from meters to feet
     apply(wh_data, (x) => x * METER_TO_FEET);
     
     wh_cache[key] = wh_data;
-    console.log("retrieved", key);
     return wh_data;
 }
